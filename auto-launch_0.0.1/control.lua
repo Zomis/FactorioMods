150275ebd5b5a3e9bf64d33cf938f919113d213d
Simon Forsberg <zomis2k@hotmail.com>
2016-05-25 18:47:19 +0200

    Add initial, laggy, version of Auto Launch

---
+++ auto-launch_0.0.1/control.lua
@@ -0,0 +1,42 @@
+require "defines"
+
+script.on_event(defines.events.on_tick, function(event)
+	if event.tick % 60 == 0 then
+		for name, force in pairs(game.forces) do
+			local silos = findType("rocket-silo", force)
+			for i, entity in ipairs(silos) do
+				launchIfReady(entity)
+			end
+		end
+	end
+end)
+
+function launchIfReady(silo)
+	if siloIsReady(silo) then
+		silo.launch_rocket()
+	end
+end
+
+function siloIsReady(silo)
+	return silo.get_item_count("satellite") > 0
+end
+
+function findType(entity_type, force)
+	if force == nil then
+		return {}
+	end
+	local results = { }
+	local surface = game.get_surface(1)
+	for coord in surface.get_chunks() do
+		local X, Y = coord.x, coord.y;
+
+		if surface.is_chunk_generated { X, Y } then
+			local area = {{X*32, Y*32}, {X*32 + 32, Y*32 + 32}}
+			for _, entity in pairs(surface.find_entities_filtered { area = area, type = entity_type, force = force.name}) do
+				table.insert(results, entity)
+			end
+		end
+	end
+	return results
+end
+
