567e7e1471ce39bc7e41196a0a1c26b4eeb0509c
Simon Forsberg <zomis2k@hotmail.com>
2016-06-06 15:02:31 +0200

    Finish extract trackentities for Auto Launch

--- auto-launch_0.0.1/control.lua
+++ auto-launch_0.0.1/control.lua
@@ -1,9 +1,10 @@
 require "defines"
+require "trackentities"
 
 script.on_event(defines.events.on_tick, function(event)
 	if event.tick % 60 == 0 then
 		for name, force in pairs(game.forces) do
-			local silos = findType("rocket-silo", force)
+			local silos = global.silos
 			for i, entity in ipairs(silos) do
 				launchIfReady(entity)
 			end
@@ -12,14 +13,13 @@ script.on_event(defines.events.on_tick, function(event)
 end)
 
 function scanAllRockets(event)
-	full_scan("silos", { "rocket-silo" })
-	
+	full_scan("silos", nil, { "rocket-silo" })
 end
 
 script.on_init(scanAllRockets)
 script.on_load(scanAllRockets)
 
-setup("silos", { "rocket-silo" })
+setupTracking("silos", { "rocket-silo" })
 
 function launchIfReady(silo)
 	if siloIsReady(silo) then
@@ -30,23 +30,3 @@ end
 function siloIsReady(silo)
 	return silo.get_item_count("satellite") > 0
 end
-
-function findType(entity_type, force)
-	if force == nil then
-		return {}
-	end
-	local results = { }
-	local surface = game.get_surface(1)
-	for coord in surface.get_chunks() do
-		local X, Y = coord.x, coord.y;
-
-		if surface.is_chunk_generated { X, Y } then
-			local area = {{X*32, Y*32}, {X*32 + 32, Y*32 + 32}}
-			for _, entity in pairs(surface.find_entities_filtered { area = area, type = entity_type, force = force.name}) do
-				table.insert(results, entity)
-			end
-		end
-	end
-	return results
-end
-
--- auto-launch_0.0.1/trackentities.lua
+++ auto-launch_0.0.1/trackentities.lua
@@ -5,29 +5,28 @@ function broadcast(str)
 end
 
 function full_scan(key, force, entity_types)
-	
+	global[key] = findTypes(entity_types, force)
 end
 
-function findType(entity_type, force)
-	if force == nil then
-		return { }
-	end
+function findTypes(entity_types, force)
 	local results = { }
 	local surface = game.get_surface(1)
-	for coord in surface.get_chunks() do
-		local X, Y = coord.x, coord.y;
+    for i, entity_type in ipairs(entity_types) do
+        for coord in surface.get_chunks() do
+            local X, Y = coord.x, coord.y;
 
-		if surface.is_chunk_generated { X, Y } then
-			local area = {{X*32, Y*32}, {X*32 + 32, Y*32 + 32}}
-			for _, entity in pairs(surface.find_entities_filtered { area = area, type = entity_type, force = force.name}) do
-				table.insert(results, entity)
-			end
-		end
-	end
+            if surface.is_chunk_generated { X, Y } then
+                local area = {{X*32, Y*32}, {X*32 + 32, Y*32 + 32}}
+                for _, entity in pairs(surface.find_entities_filtered { area = area, name = entity_type, force = force}) do
+                    table.insert(results, entity)
+                end
+            end
+        end
+    end
 	return results
 end
 
-function setup(key, entity_types) {
+function setupTracking(key, entity_types)
 	function entityBuilt(event, entity)
 		for i, entity_type in ipairs(entity_types) do
 			if entity_type == entity.name then
@@ -44,14 +43,15 @@ function setup(key, entity_types) {
 				global[key] = global[key] or {}
 				for idx, e in pairs(global[key]) do
 					if e == entity then
-						table.remove(idx)
 						broadcast("removed entity from table at index " .. tostring(idx))
+						table.remove(global[key], idx)
 					end
 				end
 			end
 		end
 	end
 
+    global[key] = global[key] or {}
 
 	script.on_event(defines.events.on_built_entity, function(event)
 		entityBuilt(event, event.created_entity)
@@ -72,5 +72,4 @@ function setup(key, entity_types) {
 	script.on_event(defines.events.on_entity_died, function(event)
 		entityMined(event, event.entity)
 	end)
-}
-
+end
