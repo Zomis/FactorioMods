55dfdb1c2a3022ba31fe3542221390be2a9a0201
Simon Forsberg <zomis2k@hotmail.com>
2017-05-26 11:57:44 +0200

    CircuitsUI: Remove some debug messages in Signal GUI

--- CircuitsUI_0.1.0/signal_gui.lua
+++ CircuitsUI_0.1.0/signal_gui.lua
@@ -1,13 +1,5 @@
-local function out(txt)
-  debug = false
-  if debug then
-    game.print(txt)
-  end
-end
-
 function CreateSignalGuiPanel(parent, signals)
   local gui = parent.add({type = "flow", direction = "horizontal", name = "signals"})
-  out("Create gui")
   
   if signals ~= nil then
     UpdateSignalGuiPanel(gui, signals)
@@ -44,7 +36,6 @@ local function UpdateSingleSignal(gui, signal)
   if typename == "virtual" then
     typename = "virtual-signal"
   end
-  out("Updating: " .. typename .. "/" .. signal.signal.name .. " value " .. signal.count)
   gui.icon.sprite = typename .. "/" .. signal.signal.name
   gui.valueLabel.caption = CountString(signal.count)
 --        local prototypes = itemSelection_prototypesForGroup(typename)
@@ -53,7 +44,6 @@ end
 
 function DestroyExcessGui(gui, count)
   while gui["signal" .. count] do
-    out("Destroying " .. count)
     gui["signal" .. count].destroy()
     count = count + 1
   end
@@ -61,19 +51,15 @@ end
 
 function UpdateSignalGuiPanel(gui, circuit_network)
   if not gui then
-    out("No gui")
     return
   end
   local count = 1
   if not circuit_network or not circuit_network.signals then
-    out("No signals")
     DestroyExcessGui(gui, count)
     return
   end
   for k, v in pairs(circuit_network.signals) do
-    out("Signal " .. v.signal.name .. " has " .. v.count)
     if not gui["signal" .. count] then
-      out("Creating " .. count)
       local signalGUI = gui.add({type = "flow", direction = "vertical", name = "signal" .. count})
       signalGUI.add({type = "sprite-button", name = "icon", style = "slot_button_style", sprite=""})
       signalGUI.add({type = "label", name = "valueLabel"})
