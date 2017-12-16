681727bb6e7a745e24bdafcbf2211f2bb8c1a8a9
Simon Forsberg <zomis2k@hotmail.com>
2017-12-16 12:57:09 +0100

    Remove parent scroll_pane_style from all scroll_pane_style

--- visual-signals_0.15.7/prototypes/ui_style.lua
+++ visual-signals_0.15.7/prototypes/ui_style.lua
@@ -1,14 +1,12 @@
 data.raw["gui-style"].default["gui_signal_display_scroll"] =
 {
     type = "scroll_pane_style",
-	parent = "scroll_pane_style",
     maximal_width = 300
 }
 
 data.raw["gui-style"].default["gui_signal_display_list"] =
 {
     type = "scroll_pane_style",
-	parent = "scroll_pane_style",
     maximal_width = 300,
     maximal_height = 400
 }
