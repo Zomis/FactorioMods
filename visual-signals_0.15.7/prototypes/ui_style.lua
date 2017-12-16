0afcf2b5d23eb7f5e453dfc18e476f2910671f5e
Simon Forsberg <zomis2k@hotmail.com>
2017-12-16 21:22:17 +0100

    Visual Signals: Make popup GUI more dynamic size

--- visual-signals_0.15.7/prototypes/ui_style.lua
+++ visual-signals_0.15.7/prototypes/ui_style.lua
@@ -7,6 +7,8 @@ data.raw["gui-style"].default["gui_signal_display_scroll"] =
 data.raw["gui-style"].default["gui_signal_display_list"] =
 {
     type = "scroll_pane_style",
-    maximal_width = 300,
-    maximal_height = 400
+    minimal_width = 300,
+    minimal_height = 400,
+    maximal_width = 800,
+    maximal_height = 800
 }
