b55ee756e8a6921c5dc3fa588e00013c97a94757
Simon Forsberg <zomis2k@hotmail.com>
2018-01-13 22:38:43 +0100

    Advanced Combinator: Remove ui_style.lua as styles are set inline

--- advanced-combinator_0.16.0/data.lua
+++ advanced-combinator_0.16.0/data.lua
@@ -1,7 +1,6 @@
 require("prototypes.advanced_combinator")
 require("prototypes.technology")
 require("prototypes.recipe")
-require("prototypes.ui_style")
 
 data.raw["gui-style"].default["advanced_combinator_scroll"] = {
     type = "scroll_pane_style",
