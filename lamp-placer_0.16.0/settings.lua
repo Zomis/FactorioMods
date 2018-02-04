17304819fed80b867075d04b60af089c8353f47d
Simon Forsberg <zomis2k@hotmail.com>
2018-02-04 02:27:45 +0100

    Lamp Placer: Add player setting for distance between lamps

---
+++ lamp-placer_0.16.0/settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+	{
+		type = "int-setting",
+		name = "lamp-placer-lamp-distance",
+		setting_type = "runtime-per-user",
+		order = "a",
+		default_value = 10,
+		minimum_value = 1,
+		maximum_value = 20
+	}
+})
