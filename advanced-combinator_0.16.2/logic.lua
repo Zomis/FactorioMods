c430c5cabd2125781865d5d84633fd77eb472450
Simon Forsberg <zomis2k@hotmail.com>
2018-02-04 16:04:20 +0100

    Advanced Combinator: Add "print" function to print message
    Allows usage of e.g. '$1' to show values from the current signals

--- advanced-combinator_0.16.2/logic.lua
+++ advanced-combinator_0.16.2/logic.lua
@@ -332,6 +332,26 @@ local logic = {
       end
     end
   },
+  print = {
+    description = "Print a message to all players in the force. Allows substitution of current signal values using $1, $2, etc.",
+    parameters = { "string" },
+    result = "command",
+    parse = function(params)
+      local param_message = params[1]
+      return function(entity, current)
+        local message = param_message
+        for search_result in string.gmatch(message, "$%d+") do
+          local search_index = tonumber(string.sub(search_result, 2))
+          local replace = "0"
+          if current[search_index] then
+            replace = current[search_index].count
+          end
+          message = string.gsub(message, search_result, replace)
+        end
+        entity.force.print(message)
+      end
+    end
+  },
   set_simple = {
     description = "At the index specified by the first parameter, set the signal of second parameter to the value returned by the third parameter",
     parameters = { "string-number", "string-signal", "number" },
