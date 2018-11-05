812fa581def14e3cba2ddf8a5b0a6d502bb1c6af
Simon Forsberg <zomis2k@hotmail.com>
2018-11-05 07:44:30 +0100

    Usage Detector: Add show unused checkbox

--- usage-detector_0.16.1/gui_center.lua
+++ usage-detector_0.16.1/gui_center.lua
@@ -9,6 +9,11 @@ local function gui_header_create(parent, job_data)
   header.add { type = "label", name = "fluid_label", caption = "or Fluid" }
   local fluid = header.add { type = "choose-elem-button", name = "fluid", elem_type = "fluid" }
 
+  if job_data.show_empty == nil then
+    job_data.show_empty = true
+  end
+  header.add({type = "checkbox", name = "show_unused", caption = "Show currently inactive recipes", state = job_data.show_empty})
+
   if job_data.current_thing then
     if job_data.current_thing.type == "item" then
       item.elem_value = job_data.current_thing.name
@@ -59,15 +64,17 @@ local function recreate_table_with_results(section, results, running_time)
   for recipe_name, result in pairs(results) do
     local name_prefix = "result_" .. recipe_name .. "_"
     local recipe_sum = result.amount * result.count
-    table.add { type = "label", name = name_prefix .. "name", caption = recipe_name }
-    table.add { type = "label", name = name_prefix .. "amount", caption = result.amount }
-    table.add { type = "label", name = name_prefix .. "count", caption = result.count }
-    table.add { type = "label", name = name_prefix .. "sum", caption = recipe_sum }
-    table.add { type = "label", name = name_prefix .. "machine_count", caption = result.machine_count }
-    local used_per_second = recipe_sum / running_seconds
-    table.add { type = "label", name = name_prefix .. "per_second", caption = round_to_closest(used_per_second, 0.01) }
-    local used_percent = recipe_sum / all_sum * 100
-    table.add { type = "label", name = name_prefix .. "percent", caption = round_to_closest(used_percent, 0.01) }
+    if recipe_sum > 0 or section.header.show_unused.state then
+      table.add { type = "label", name = name_prefix .. "name", caption = recipe_name }
+      table.add { type = "label", name = name_prefix .. "amount", caption = result.amount }
+      table.add { type = "label", name = name_prefix .. "count", caption = result.count }
+      table.add { type = "label", name = name_prefix .. "sum", caption = recipe_sum }
+      table.add { type = "label", name = name_prefix .. "machine_count", caption = result.machine_count }
+      local used_per_second = recipe_sum / running_seconds
+      table.add { type = "label", name = name_prefix .. "per_second", caption = round_to_closest(used_per_second, 0.01) }
+      local used_percent = recipe_sum / all_sum * 100
+      table.add { type = "label", name = name_prefix .. "percent", caption = round_to_closest(used_percent, 0.01) }
+    end
   end
 end
 
