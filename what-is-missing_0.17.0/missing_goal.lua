f4623c313d774e037fe657b9a00deb031972320a
Simon Forsberg <zomis2k@hotmail.com>
2019-12-21 23:46:32 +0100

    Improve total requirements calculation to also consider technologies

--- what-is-missing_0.17.0/missing_goal.lua
+++ what-is-missing_0.17.0/missing_goal.lua
@@ -1,66 +1,3 @@
---[[
-class Item {
-  constructor(public count: number, public name: string) {}
-}
-
-class Recipe {
-  constructor(public inputs: Array<Item>, public result: Item) {}
-
-  toString(): string {
-    return `${this.inputs} => ${this.result}`
-  }
-}
-
-function parseItem(item: string): Item {
-  let split = item.split(" ")
-  return new Item(parseInt(split[0], 10), split[1])
-}
-
-function parseRecipe(line: string): Recipe {
-  let split = line.split(" => ")
-  let ingredients = split[0].split(", ")
-  let result = split[1]
-  return new Recipe(ingredients.map(parseItem), parseItem(result))
-}
-
-class Recipes {
-  public recipes: Array<Recipe> = new Array<Recipe>()
-
-  add(line: string) {
-    this.recipes.push(parseRecipe(line.trim()))
-  }
-
-  findRecipeFor(name: string): Recipe | undefined {
-    return this.recipes.find((r: Recipe) => r.result.name == name)
-  }
-
-}
-
-function itemIsMissing(item: Item): boolean {
-  return item.count < 0 && item.name != "ORE"
-}
-
-  part2(input: Recipes): any {
-    let have = new Array<Item>()
-    let requiredForOne = this.part1(input) as number
-    have.push(new Item(1000000000000, "ORE"))
-    let totalFuelAdded = 0
-    while (this.resourceCount(have, "ORE") > 0) {
-      let moreFuel = Math.floor(this.resourceCount(have, "ORE") / requiredForOne)
-      if (moreFuel == 0) {
-        moreFuel = 1
-      }
-      totalFuelAdded += moreFuel
-
-      modify(have, "FUEL", -moreFuel)
-      have = this.resolveRequirements(input, have)
-    }
-    return totalFuelAdded - 1;// this.resourceCount(have, "FUEL");
-  }
-
-}
---]]
-
 Inventory = {}
 Inventory.__index = Inventory
 
@@ -71,19 +8,21 @@ local function productAmount(product)
     return product.probability * (product.amount_max + product.amount_min) / 2
 end
 
-local function applyRecipe(recipe, inventory, want)
-    recipe.force.print("Applying recipe: " .. recipe.name)
-    local product = nil
-    for _, result in pairs(recipe.products) do
-        if (result.type == want.type and result.name == want.name) then
-            product = result
+local function applyRecipe(force, recipe, inventory, want)
+    local times = 1
+    if recipe.type ~= "technology" then
+        local product = nil
+        for _, result in pairs(recipe.products) do
+            if (result.type == want.type and result.name == want.name) then
+                product = result
+            end
         end
-    end
-    if not product then
-        error({message = "Trying to apply the incorrect recipe to get " .. want})
+        if not product then
+            error({message = "Trying to apply the incorrect recipe to get " .. want.name})
+        end
+        times = math.ceil(math.abs(want.count) / productAmount(product))
     end
     local results = inventory:copy()
-    local times = math.ceil(math.abs(want.count) / product.amount)
     for _, i in pairs(recipe.ingredients) do
         results:modify(i.type, i.name, -i.amount * times)
     end
@@ -93,10 +32,11 @@ local function applyRecipe(recipe, inventory, want)
     return results
 end
 
-function Inventory:new()
+function Inventory:new(force)
     local obj = {}
     setmetatable(obj, Inventory)
     obj.data = {}
+    obj.force = force
     return obj
 end
 
@@ -110,14 +50,12 @@ end
 
 function Inventory:modify(type, itemName, count)
     local existing = self.data[type .. "/" .. itemName]
---    local existing = find(self.data, function(i) return i.name == itemName end)
     if (existing ~= nil) then
         if existing.type ~= "technology" or count > 0 then -- don't remove a technology once it is researched
             existing.count = existing.count + count
         end
     else
         self.data[type .. "/" .. itemName] = {type = type, name = itemName, count = count}
-        --table.insert(self.data, { name = name, count = count })
     end
 end
   
@@ -130,7 +68,16 @@ function Inventory:hasMissingItems()
     return false
 end
 
-local function findRecipeFor(force, product)
+function Inventory:findRecipeFor(force, product)
+    if product.type == "recipe" then
+        for _, tech in pairs(force.technologies) do
+            for _, modifier in pairs(tech.effects) do
+                if modifier.type == "unlock-recipe" and modifier.recipe == product.name then
+                    return self:findRecipeFor(force, {type = "technology", name = tech.name})
+                end
+            end
+        end
+    end
     if product.type == "technology" then
         local recipe = force.technologies[product.name]
         local count = recipe.research_unit_count
@@ -140,25 +87,41 @@ local function findRecipeFor(force, product)
         local ingredients = {}
         for _, v in pairs(research_ingredients) do
             -- TODO: Detect and ignore recipes that cause loops
-            table.insert(ingredients, {type = v.type, name = v.name, amount = v.amount * count}) -- TODO: Consider Ingredient.catalyst_amount ?
+            -- TODO: Consider Ingredient.catalyst_amount ?
+            table.insert(ingredients, {type = v.type, name = v.name, amount = v.amount * count})
         end
-        for _, prerequisite in pairs(prerequisites) do
+        for _, prerequisite in pairs(recipe.prerequisites) do
             table.insert(ingredients, {type = "technology", name = prerequisite.name, amount = 1 })
         end
+        local products = { { type = "technology", name = recipe.name, amount = 1 } }
+        for _, modifier in pairs(recipe.effects) do
+            if modifier.type == "unlock-recipe" then
+                table.insert(products, {type = "recipe", name = modifier.recipe, amount = 0})
+            end
+        end
 
-        -- time, research ingredients, prerequisites
+        -- TODO: Add time
         return {
+            type = "technology",
             name = recipe.name,
-            time = time,
             ingredients = ingredients,
-            products = { { type = "technology", name = recipe.name, amount = 1 } }
+            products = products
         }
     end
     if product.type == "item" or product.type == "fluid" then
         for k, recipe in pairs(force.recipes) do
             for _, recipe_product in pairs(recipe.products) do
                 if recipe_product.type == product.type and recipe_product.name == product.name then
-                    return recipe
+                    if not recipe.enabled and not self.data["recipe/" .. recipe.name] then
+--                        force.print("Recipe " .. recipe.name .. " is not available yet" .. ". " .. math.random(1, 10))
+                        return self:findRecipeFor(force, {type = "recipe", name = recipe.name})
+                    end
+                    return {
+                        type = "recipe",
+                        name = recipe.name,
+                        ingredients = recipe.ingredients,
+                        products = recipe.products
+                    }
                 end
             end
         end
@@ -169,7 +132,7 @@ end
 
 function Inventory:findMissingItem(force)
     for _, v in pairs(self.data) do
-        if v.count < 0 and findRecipeFor(force, v) then
+        if v.count < 0 and self:findRecipeFor(force, v) then
             return v
         end
     end
@@ -177,7 +140,7 @@ function Inventory:findMissingItem(force)
 end
 
 function Inventory:copy()
-    local newInventory = Inventory:new()
+    local newInventory = Inventory:new(self.force)
     for k, v in pairs(self.data) do
         newInventory:modify(v.type, v.name, v.count)
     end
@@ -186,14 +149,15 @@ end
 
 function Inventory:resolveRequirements(force)
   local have = self:copy()
-  while true do
+  local tries = 0
+  while tries < 100000 do
+    tries = tries + 1
     local product = have:findMissingItem(force)
     if product == nil then
         return have
     end
-    local recipe = findRecipeFor(force, product)
---    force.print("Found recipe " .. recipe.name)
-    have = applyRecipe(recipe, have, product)
+    local recipe = have:findRecipeFor(force, product)
+    have = applyRecipe(force, recipe, have, product)
   end
   return have
 end
@@ -208,11 +172,13 @@ function Inventory:resourceCount(name)
 end
 
 local function MissingGoal(player)
-  local have = Inventory:new()
+  local have = Inventory:new(player.force)
   have:modify("item", "assembling-machine-1", -1)
+--  have:modify("item", "rocket-part", -100)
+--  have:modify("item", "satellite", -1)
   have = have:resolveRequirements(player.force)
   for k, v in pairs(have.data) do
-    if v.count ~= 0 then
+    if v.count ~= 0 and v.type ~= "technology" then
         player.print(v.name .. ": " .. v.count)
     end
   end
