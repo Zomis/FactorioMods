fdfae9f6f558444bf5fd798213f247a806ee6f2d
Simon Forsberg <zomis2k@hotmail.com>
2020-05-07 19:21:05 +0200

    Lamp Placer: Make async

--- lamp-placer_0.18.0/control.lua
+++ lamp-placer_0.18.0/control.lua
@@ -1,3 +1,5 @@
+local Async = require "async"
+
 local function droppedItem(event)
     if not event.entity or not event.entity.stack then
         return
@@ -16,16 +18,17 @@ end
 
 local function findElectricity(poles, x, y)
     for _, entity in ipairs(poles) do
-        if isInRange(entity.position, x, y, entity.prototype.supply_area_distance) then
+        if entity.valid and isInRange(entity.position, x, y, entity.prototype.supply_area_distance) then
             return true
         end
     end
     return false
 end
 
-local function findLamp(lamps, x, y, lamp_distance)
-    for _, entity in ipairs(lamps) do
-        if isInRange(entity.position, x, y, lamp_distance) then
+local function findLamp(lamp_positions, x, y, lamp_distance)
+    for _, position in ipairs(lamp_positions) do
+        -- Do not perform valid check because ghosts that get placed are no longer valid. All we are interested in is the positions
+        if isInRange(position, x, y, lamp_distance) then
             return true
         end
     end
@@ -49,48 +52,69 @@ local function on_player_selected_area(event)
     local searchEntities = surface.find_entities({ { event.area.left_top.x - extra, event.area.left_top.y - extra },
       { event.area.right_bottom.x + extra, event.area.right_bottom.y + extra } })
 
-    local lamps = {}
-
     local electricity = {}
+    local lamp_positions = {}
     for _, entity in ipairs(searchEntities) do
         if entity.type == "electric-pole" then
             table.insert(electricity, entity)
         end
         if entity.type == "lamp" then
-            table.insert(lamps, entity)
+            table.insert(lamp_positions, entity.position)
         end
     end
 
-    local placables = 0
-    local electricities = 0
-    local noLamps = 0
-    local force = player.force
-    for x = round(event.area.left_top.x) + 0.5, round(event.area.right_bottom.x) - 0.5 do
-        for y = round(event.area.left_top.y) + 0.5, round(event.area.right_bottom.y) - 0.5 do
-            local position = { x = x, y = y }
-            local placable = surface.can_place_entity({ name = "small-lamp", position = position, force = force })
-            if placable then
-                placables = placables + 1
-                -- Check if there is electricity in area
-                local electricityFound = findElectricity(electricity, x, y)
-                if electricityFound then
-                    electricities = electricities + 1
-                    -- Check if there is an existing lamp in area
-                    local lampFound = findLamp(lamps, x, y, lamp_distance)
-                    if not lampFound then
-                        noLamps = noLamps + 1
-                        local newLamp = surface.create_entity({ name = "entity-ghost",
-                          inner_name = "small-lamp", expires = false, position = { x, y }, force = force })
-                        table.insert(lamps, newLamp)
-                    end
-                end
-            end
-        end
-    end
-    player.print("Placed " .. noLamps .. " lamps on " .. placables ..
-     " placable positions. " .. electricities .. " of which had electricity.")
+    local width = round(event.area.right_bottom.x) - round(event.area.left_top.x)
+    local height = round(event.area.right_bottom.y) - round(event.area.left_top.y)
+    local xloop = Async:loop("x", round(event.area.left_top.x) + 0.5, round(event.area.right_bottom.x) - 0.5)
+    local yloop = Async:loop("y", round(event.area.left_top.y) + 0.5, round(event.area.right_bottom.y) - 0.5)
+    local task_data = {
+        lamp_positions = lamp_positions, electricity = electricity,
+        placables = 0, electricities = 0, lamps_placed = 0,
+        lamp_distance = lamp_distance,
+        player = player, force = player.force, surface = player.surface
+    }
+    local place = Async:perform_once(task_data, { xloop, yloop })
+    player.print("[Lamp Placer] Starting placing lamps in an area of " .. width .. " x " .. height .. " using distance of " .. lamp_distance)
 end
 
+local function async_place_lamp(values, task_data)
+    local x = values.x
+    local y = values.y
+    local position = { x = x, y = y }
+    local placable = task_data.surface.can_place_entity({ name = "small-lamp", position = position, force = task_data.force })
+    if not placable then
+        return
+    end
+    task_data.placables = task_data.placables + 1
+    -- Check if there is electricity in area
+    local electricityFound = findElectricity(task_data.electricity, x, y)
+    if not electricityFound then
+        return
+    end
+    task_data.electricities = task_data.electricities + 1
+    -- Check if there is an existing lamp in area
+    local lampFound = findLamp(task_data.lamp_positions, x, y, task_data.lamp_distance)
+    if not lampFound then
+        task_data.lamps_placed = task_data.lamps_placed + 1
+        local newLamp = task_data.surface.create_entity({ name = "entity-ghost",
+            inner_name = "small-lamp", expires = false, position = position, force = task_data.force })
+        table.insert(task_data.lamp_positions, newLamp.position)
+    end
+end
+
+local function async_place_lamp_finished(task_data)
+    task_data.player.print("[Lamp Placer] Placed " .. task_data.lamps_placed .. " lamps on " .. task_data.placables .. " placable positions. " .. task_data.electricities .. " of which had electricity.")
+end
+
+Async:configure(function(task)
+    return { perform_function = async_place_lamp, on_finished = async_place_lamp_finished }
+end)
+
+local function on_tick(event)
+    Async:on_tick()
+end
+
+script.on_event(defines.events.on_tick, on_tick)
 script.on_event(defines.events.on_player_selected_area, on_player_selected_area)
 script.on_event(defines.events.on_player_alt_selected_area, on_player_selected_area)
 script.on_event(defines.events.on_player_dropped_item, droppedItem)
