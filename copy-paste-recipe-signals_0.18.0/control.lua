758702b1b86a6643fb8c9257b0427b432813ab18
Simon Forsberg <zomis2k@hotmail.com>
2020-10-08 21:54:08 +0200

    Copy-Paste Recipe Signals: Add player-specific settings

--- copy-paste-recipe-signals_0.18.0/control.lua
+++ copy-paste-recipe-signals_0.18.0/control.lua
@@ -13,21 +13,34 @@ script.on_event(defines.events.on_entity_settings_pasted, function(event)
     return
   end
   if event.source.valid and event.source.prototype.type == "assembling-machine" then
+    local player_settings = settings.get_player_settings(event.player_index)
+    local ingredients_multiplier = player_settings["copy-paste-recipe-signals-ingredient-multiplier"].value
+    local products_multiplier = player_settings["copy-paste-recipe-signals-product-multiplier"].value
+    local add_ticks = player_settings["copy-paste-recipe-signals-include-ticks"].value
+    local add_seconds = player_settings["copy-paste-recipe-signals-include-seconds"].value
+
     local recipe = event.source.get_recipe()
     if not recipe then return end
-
     local behavior = event.destination.get_or_create_control_behavior()
 
     local signals = {}
-    for _, ingredient in pairs(recipe.ingredients) do
-      table.insert(signals, { signal = { type = ingredient.type, name = ingredient.name }, count = -ingredient.amount })
+    if ingredients_multiplier ~= 0 then
+      for _, ingredient in pairs(recipe.ingredients) do
+        table.insert(signals, { signal = { type = ingredient.type, name = ingredient.name }, count = ingredient.amount * ingredients_multiplier })
+      end
     end
-    for _, product in pairs(recipe.products) do
-      local expected = expected_amount(product)
-      table.insert(signals, { signal = { type = product.type, name = product.name }, count = expected })
+    if products_multiplier ~= 0 then
+      for _, product in pairs(recipe.products) do
+        local expected = expected_amount(product)
+        table.insert(signals, { signal = { type = product.type, name = product.name }, count = expected * products_multiplier })
+      end
     end
-    table.insert(signals, { signal = { type = "virtual", name = "signal-T" }, count = recipe.energy * 60 })
-    table.insert(signals, { signal = { type = "virtual", name = "signal-S" }, count = recipe.energy })
+    if add_ticks then
+      table.insert(signals, { signal = { type = "virtual", name = "signal-T" }, count = recipe.energy * 60 })
+    end
+    if add_seconds then
+      table.insert(signals, { signal = { type = "virtual", name = "signal-S" }, count = recipe.energy })
+    end
 
     for index, signal in pairs(signals) do
       if behavior.signals_count >= index then
