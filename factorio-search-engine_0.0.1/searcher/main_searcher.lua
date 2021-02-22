0da7311911c2efd977c2dc457d8b8d5913326620
Simon Forsberg <zomis2k@hotmail.com>
2021-02-22 22:35:00 +0100

    Search Engine: Add support for fluid containers

--- factorio-search-engine_0.0.1/searcher/main_searcher.lua
+++ factorio-search-engine_0.0.1/searcher/main_searcher.lua
@@ -2,6 +2,8 @@ local searcher = require "searcher/searcher"
 local main_searcher = searcher:new()
 main_searcher:add_type_plugin(require "plugins/items/items")
 main_searcher:add_search_plugin(require "plugins/items/item_container")
+main_searcher:add_type_plugin(require "plugins/fluids/fluids")
+main_searcher:add_search_plugin(require "plugins/fluids/fluids_container")
 
 main_searcher:add_type_plugin(require "plugins/signals/signals")
 main_searcher:add_search_plugin(require "plugins/signals/signals_search")
