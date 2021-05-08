01c343d5b1655ef3dbb1cb37392ff36956bd5bfe
Simon Forsberg <zomis2k@hotmail.com>
2021-05-08 17:55:32 +0200

    Train Log: Make sure that mod gui button is always added

--- train-log_1.1.2/migration.lua
+++ train-log_1.1.2/migration.lua
@@ -1,18 +1,14 @@
 local events = require("__flib__.event")
-local migration = require("__flib__.migration")
 local mod_gui_button = require("gui/mod_gui_button")
 
-local migrations = {
-    ["1.0.0"] = function()
-        for _, player in pairs(game.players) do
-            if player.valid then
-                mod_gui_button.add_mod_gui_button(player)
-            end
+local function add_mod_gui_buttons()
+    for _, player in pairs(game.players) do
+        if player.valid then
+            mod_gui_button.add_mod_gui_button(player)
         end
     end
-}
+end
 
 events.on_configuration_changed(function(e)
-    -- this does return true or false, but we don't care about the result at this time.
-    migration.on_config_changed(e, migrations)
+    add_mod_gui_buttons()
 end)
