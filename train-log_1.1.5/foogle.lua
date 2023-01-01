253f53529d4cce00cd862eb2ae81314e145b900f
Simon Forsberg <zomis2k@hotmail.com>
2023-01-01 23:39:51 +0100

    Train Log: Add custom Foogle button

--- train-log_1.1.5/foogle.lua
+++ train-log_1.1.5/foogle.lua
@@ -12,6 +12,13 @@ remote.add_interface("train-log", {
 local function on_load()
     if remote.interfaces["foogle"] then
         remote.call("foogle", "add_integration", "train-log", {
+            button = {
+                type = "sprite-button",
+                name = "train_log",
+                style = "slot_button",
+                sprite = "train_log_train-36-white",
+                tooltip = { "train-log.mod-gui-tooltip" }
+            },
             supported_check = "foogle_support",
             callback = "open_train_log"
         })
