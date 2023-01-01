92c1ec99cb507c88ea7959cf0dbf1f28e67afb67
Simon Forsberg <zomis2k@hotmail.com>
2023-01-01 20:56:59 +0100

    Foogle: Show icons for some selection options

--- foogle_0.0.1/gui/selection_options.lua
+++ foogle_0.0.1/gui/selection_options.lua
@@ -1,34 +1,81 @@
 local guis = require("__flib__.gui")
+local tables = require("__flib__.table")
 local header = require("gui/header")
+
+local function add_inventory(contents, inventory)
+    for item in pairs(inventory.get_contents()) do
+        contents[item] = true
+    end
+end
+
+local function add_fluids(fluids, entity)
+    for fluid in pairs(entity.get_fluid_contents()) do
+        fluids[fluid] = true
+    end
+end
 
 local function scan_entities(entities_to_scan)
     local entities = {}
     local contents = {}
+    local fluids = {}
     local settings = {}
     local signals = {}
-    local text = {}
     for k, entity in pairs(entities_to_scan) do
-        text["TYPE: " .. entity.type] = "TYPE: " .. entity.type
-        text[entity.name] = entity.name
+        table.insert(entities, entity)
         if entity.type == "character" then
+            add_inventory(contents, entity.get_main_inventory())
+        end
+        if entity.type == "container" then
+            add_inventory(contents, entity.get_inventory(defines.inventory.chest))
+        end
+        if entity.type == "storage-tank" then
+            add_fluids(fluids, entity)
+        end
+        if entity.type == "pipe" or entity.type == "pipe-to-ground" then
+            add_fluids(fluids, entity)
         end
     end
 
     return {
         entities = entities,
         contents = contents,
+        fluids = fluids,
         settings = settings,
         signals = signals,
-        text = text
     }
 end
 
 local function build_ui(groups)
     local results = {}
-    for _, text in pairs(groups.text) do
+    table.insert(results, {
+        type = "table",
+        column_count = 10,
+        children = tables.map(groups.entities, function(v)
+            return {
+                type = "sprite-button",
+                sprite = "entity/" .. v.name
+            }
+        end)
+    })
+    
+    table.insert(results, {
+        type = "label",
+        caption = "CONTENTS"
+    })
+    for thing in pairs(groups.contents) do
         table.insert(results, {
-            type = "label",
-            caption = text
+            type = "sprite-button",
+            sprite = "item/" .. thing
+        })
+    end
+    table.insert(results, {
+        type = "label",
+        caption = "FLUIDS"
+    })
+    for thing in pairs(groups.fluids) do
+        table.insert(results, {
+            type = "sprite-button",
+            sprite = "fluid/" .. thing
         })
     end
     return results
@@ -49,9 +96,18 @@ local function show_entities(player, entities)
             children = {
                 header({ "foogle.selection-title" }),
                 {
-                    type = "flow",
-                    direction = "vertical",
-                    children = build_ui(result_groups)
+                    type = "scroll-pane",
+                    style = "flib_naked_scroll_pane_no_padding",
+                    ref = { "scroll_pane" },
+                    vertical_scroll_policy = "always",
+                    style_mods = {width = 650, height = 400, padding = 6},
+                    children = {
+                        {
+                            type = "flow",
+                            direction = "vertical",
+                            children = build_ui(result_groups)
+                        },
+                    }
                 }
             }
         }
